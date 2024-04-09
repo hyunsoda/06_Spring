c1800857686d1591c9ce89ff030ea42a13148656
hyunsoda <so0144@naver.com>
2024-04-09 12:51:43 +0900

    fix

--- todoList-boot/src/main/java/edu/kh/todo/controller/AjaxController.java
+++ todoList-boot/src/main/java/edu/kh/todo/controller/AjaxController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -153,5 +154,20 @@ public class AjaxController {
 	}
 	
 	
+	// 완료 여부 변경
+	@ResponseBody
+	@PutMapping("changeComplete")
+	public int changeComplete(@RequestBody Todo todo) { // 자동으로 mapping 및 setting
+		
+		return service.changeComplete(todo);
+	}
+	
+	
+	// 할 일 수정
+	@ResponseBody
+	@PutMapping("update")
+	public int todoUpdate(@RequestBody Todo todo) {
+		return service.todoUpdate(todo);
+	}
 	
 }
