6b0265916c00ec2719895fe6e4a47563a8a94d35
hyunsoda <so0144@naver.com>
2024-04-18 17:41:34 +0900

    fix

--- boardProject-Boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ boardProject-Boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -1,7 +1,9 @@
 package edu.kh.project.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -11,6 +13,18 @@ public class MainController {
 		
 		// 접두사 / 접미사 제외
 		return "common/main";
+	}
+	
+	
+	// LoginFilter  -> loginError 리다이렉트
+	// -> message 만들어서 메인페이지로 리다이렉트
+	// localhost/loginError로 보낼 것이기 때문에 공통주소 매핑하지 않은 컨트롤러에 작성
+	@GetMapping("loginError")
+	public String loginError(RedirectAttributes ra) {
+		
+		ra.addFlashAttribute("message", "로그인 후 이용해 주세요");
+		
+		return "redirect:/";
 	}
 	
 	
