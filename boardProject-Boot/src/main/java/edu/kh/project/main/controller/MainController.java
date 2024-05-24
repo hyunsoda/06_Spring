ae0585d268836db51c4d6d90a24ff81e50a7b015
hyunsoda <so0144@naver.com>
2024-05-24 17:31:03 +0900

    fix

--- boardProject-Boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ boardProject-Boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -1,13 +1,20 @@
 package edu.kh.project.main.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@PropertySource("classpath:/config.properties")  // config.properties 파일 이용
 public class MainController {
 
+	@Value("${my.public.data.service.key.decode}")
+	private String decodeServiceKey;
+	
 	@RequestMapping("/") // "/" 요청 매핑(method 가리지 않음)
 	public String mainPage() {
 		
@@ -25,6 +32,16 @@ public class MainController {
 		ra.addFlashAttribute("message", "로그인 후 이용해 주세요");
 		
 		return "redirect:/";
+	}
+	
+	/* 공공데이터
+	 * 서비스 키 리턴하기
+	 * */
+	@GetMapping("/getServiceKey")
+	@ResponseBody
+	public String getServiceKey() {
+		
+		return decodeServiceKey;
 	}
 	
 	
